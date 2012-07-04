84cb857c97cd857d50adc19c200118a55a6f6081
Mats Alm <mats.alms.epost@gmail.com>
2012-07-04 22:39:17 +0200

    Started work on circular refs

--- src/ExcelFormulaParser.Tests/IntegrationTests/ExcelDataProviderTests/ExcelDataProviderIntegrationTests.cs
+++ src/ExcelFormulaParser.Tests/IntegrationTests/ExcelDataProviderTests/ExcelDataProviderIntegrationTests.cs
@@ -36,5 +36,20 @@ namespace ExcelFormulaParser.Tests.IntegrationTests.ExcelDataProviderTests
             var result = parser.Parse(string.Format("=sum({0})", expectedAddres));
             Assert.AreEqual(4d, result);
         }
+
+        [TestMethod, Ignore]
+        public void ShouldHandleCircularReference()
+        {
+            // This Test is currently causing an infinite loop due to circular reference.
+            // TODO fix this...
+            var expectedAddres = "A1:A2";
+            var provider = MockRepository.GenerateStub<ExcelDataProvider>();
+            provider
+                .Stub(x => x.GetRangeValues(expectedAddres))
+                .Return(new object[] { 1, "=SUM(A1:A2)" });
+            var parser = new FormulaParser(provider);
+            var result = parser.Parse(string.Format("=sum({0})", expectedAddres));
+            Assert.AreEqual(4d, result);
+        }
     }
 }
