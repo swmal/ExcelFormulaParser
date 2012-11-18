7e0ee1e32e6c30a3a4489ffd6d378b37b1b48aae
Mats Alm <mats.alms.epost@gmail.com>
2012-11-18 22:57:49 +0100

    Started work on ValueMatcher

--- src/ExcelFormulaParser.Engine/Excel/Functions/RefAndLookup/VLookup.cs
+++ src/ExcelFormulaParser.Engine/Excel/Functions/RefAndLookup/VLookup.cs
@@ -24,25 +24,6 @@ namespace ExcelFormulaParser.Engine.Excel.Functions.RefAndLookup
             }
             while (navigator.MoveNext());
 
-
-            //var factory = new RangeAddressFactory(context.ExcelDataProvider);
-            //var rangeAddress = factory.Create(lookupArgs.RangeAddress);
-            //var fromRow = rangeAddress.FromRow;
-            //var col = rangeAddress.FromCol;
-            //for (var row = fromRow; row <= rangeAddress.ToRow; row++)
-            //{
-            //    var cellValue = context.ExcelDataProvider.GetCellValue(row, col);
-            //    if (cellValue != null && IsMatch(cellValue.Value, lookupArgs.SearchedValue))
-            //    {
-            //        // colIndex is one-based
-            //        var column = col + lookupArgs.ColumnIndex - 1;
-            //        var correspondingVal = context.ExcelDataProvider.GetCellValue(row, column);
-            //        if (correspondingVal != null)
-            //        {
-            //            return CreateResult(correspondingVal.Value, DataType.String);
-            //        }
-            //    }
-            //}
             return CreateResult(null, DataType.String);
         }
     }
---
+++ src/ExcelFormulaParser.Engine/ExcelUtilities/ValueMatcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelFormulaParser.Engine.ExcelUtilities
+{
+    public class ValueMatcher
+    {
+        public virtual int IsMatch(object o1, object o2)
+        {
+            if (o1 != null && o2 == null) return 1;
+            if (o1 == null && o2 != null) return -1;
+            if (o1 == null && o2 == null) return 0;
+            if (o1.GetType() == typeof(string) && o2.GetType() == typeof(string))
+            {
+                return o1.ToString().CompareTo(o2);
+            }
+            else if( o1.GetType() == typeof(string))
+            {
+                // todo... compare
+            }
+            else if (o2.GetType() == typeof(string))
+            {
+                // todo.. compare
+            }
+            return Convert.ToDouble(o1).CompareTo(Convert.ToDouble(o2));
+        }
+    }
+}
